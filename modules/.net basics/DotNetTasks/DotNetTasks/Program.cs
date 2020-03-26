a24209df01a06e3244ef9c043ac64a658f486579
lsk <vadym.lysenko@nixsolutions.com>
2020-03-26 20:40:29 +0200

    Added "FilesAndStreams" Task

---
+++ modules/.net basics/DotNetTasks/DotNetTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DotNetTasks.Tasks.Task1;
+
+namespace DotNetTasks
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var taskFilesAndStreams = new FilesAndStreams();
+
+            Console.WriteLine("Enter data via enter 10 times:");
+            var dataInMemory = taskFilesAndStreams.ReadConsoleInputToMemory();
+
+            Console.WriteLine("Writing data from memory to file..");
+            taskFilesAndStreams.WriteMemoryStreamToFileStream(dataInMemory);
+            
+            Console.WriteLine("Reading data from file to console..");
+            var result = taskFilesAndStreams.ReadFileOutput();
+
+            Console.Write($"Result: \n{result}");
+            Console.Read();
+        }
+    }
+}
---
+++ modules/.net basics/DotNetTasks/DotNetTasks/Tasks/Task1/FilesAndStreams.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DotNetTasks.Tasks.Task1
+{
+    public class FilesAndStreams
+    {
+        private readonly string _path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "file.txt");
+
+        public MemoryStream ReadConsoleInputToMemory()
+        {
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+
+            for (var i = 0; i < 10; i++)
+            {
+                var line = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    streamWriter.WriteLine(line);
+                }
+            }
+
+            streamWriter.Flush();
+            memoryStream.Position = 0;
+
+            return memoryStream;
+        }
+
+        public void WriteMemoryStreamToFileStream(MemoryStream memoryStream)
+        {
+            var fileStream = new FileStream(this._path, FileMode.Create);
+
+            memoryStream.CopyTo(fileStream);
+
+            fileStream.Flush();
+            fileStream.Close();
+            memoryStream.Close();
+        }
+
+        public string ReadFileOutput()
+        {
+            using var readonlyFileStream = new FileStream(this._path, FileMode.Open, FileAccess.Read);
+
+            byte[] buffer;
+
+            try
+            {
+                var length = (int)readonlyFileStream.Length;
+                buffer = new byte[length];
+                int count;
+                var offset = 0;
+
+                while ((count = readonlyFileStream.Read(buffer, offset, length - offset)) > 0)
+                    offset += count;
+            }
+            finally
+            {
+                readonlyFileStream.Close();
+            }
+
+            return Encoding.UTF8.GetString(buffer);
+        }
+    }
+}
