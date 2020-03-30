5f3d5eb59b4a77bd765ea390301400ad279f8d4e
lsk <vadym.lysenko@nixsolutions.com>
2020-03-30 16:42:27 +0300

    Added MultiThreading Task

--- modules/.net basics/DotNetTasks/DotNetTasks/Program.cs
+++ modules/.net basics/DotNetTasks/DotNetTasks/Program.cs
@@ -1,7 +1,6 @@
-﻿using DotNetTasks.Tasks.Task3;
-using DotNetTasks.Tasks.Task3.Entities;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.Threading.Tasks;
+using DotNetTasks.Tasks.Task4;
 
 namespace DotNetTasks
 {
@@ -9,58 +8,14 @@ namespace DotNetTasks
     {
         private static void Main(string[] args)
         {
-            var customer = new Customer
-            {
-                Name = "CustomerName",
-                DateTimeRegistration = DateTime.Now,
-                Id = 1
-            };
-
-            var customer2 = new Customer2
-            {
-                Name = "CustomerName2",
-                DateTimeRegistration = DateTime.Now,
-                Id = 2
-            };
-
-            IEnumerable<Order> orders = new List<Order>
-            {
-                new Order
-                {
-                    Id = 1,
-                    Name = "Order1",
-                    DateTimeOrdered = DateTime.Now
-                },
-                new Order
-                {
-                    Id = 2,
-                    Name = "Order2",
-                    DateTimeOrdered = DateTime.Now
-                },
-                new Order
-                {
-                    Id = 3,
-                    Name = "Order3",
-                    DateTimeOrdered = DateTime.Now
-                }
-            };
-
-            customer.Orders.AddRange(orders);
-            customer2.Orders.AddRange(orders);
-
-            var serialization = new Serialization();
-
-            var json = serialization.SerializeJson(customer);
-            var customerJson = serialization.DeserializeJson(json);
+            Console.WriteLine("Main start");
+            var multiThreading = new MultiThreading();
             
-            var json2 = serialization.SerializeJsonAndWriteToFile(customer2);
-            var customerJson2 = serialization.DeserializeJson(json2);
-
-            serialization.SerializeXml(customer);
-            var customerXml = serialization.DeserializeXml(customer);
-
-            serialization.SerializeBinary(customer);
-            var customerBinary = serialization.DeserializeBinary();
+            Console.WriteLine("FileRead start");
+            var fileReadTask = Task.Factory.StartNew(multiThreading.FileTrigger);
+            
+            Console.WriteLine(fileReadTask.Result);
+            Console.WriteLine("FileRead end");
 
             Console.Read();
         }
---
+++ modules/.net basics/DotNetTasks/DotNetTasks/Tasks/Task4/Multithreading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DotNetTasks.Tasks.Task4
+{
+    public class MultiThreading
+    {
+        public string FileTrigger()
+        {
+            while (true)
+            {
+                using var fileStream = File.OpenRead("file.txt");
+                Console.WriteLine("Read file...");
+
+                if (fileStream.Length == 0)
+                {
+                    fileStream.Close();
+
+                    Console.Beep(1000, 100);
+                    Thread.Sleep(5000);
+                }
+                else
+                {
+                    var buffer = new byte[fileStream.Length];
+                    fileStream.Read(buffer, 0, buffer.Length);
+
+                    return Encoding.UTF8.GetString(buffer);
+                }
+            }
+        }
+    }
+}
+
